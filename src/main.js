7d1bd07aa0d33bcd203e1379b86d3ba92b5eb52f
Vidit Shah <vidit0210@protonmail.com>
2022-07-09 01:37:09 +0530

    tailwind config

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import App from "@/App.vue";
+import "@/assets/tailwind.css";
 import { createApp } from "vue";
 createApp(App).mount("#app");
---
+++ src/playground.js
@@ -0,0 +1,5 @@
+const fruits = ["apple", "banana", "orange"];
+const vegetables = ["carrot", "tomato", "potato"];
+
+const mixAll = [...fruits, ...vegetables];
+console.log("🚀 ~ file: playground.js ~ line 5 ~ mixAll", mixAll);
