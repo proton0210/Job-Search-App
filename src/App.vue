e57d219835bf2908336177386204904fc5fe4644
Vidit Shah <vidit0210@protonmail.com>
2022-07-09 15:27:06 +0530

    Main Nav component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <h1 class="mt-8 text-4xl text-center">Hello</h1>
+  <MainNav />
 </template>
 
 <script>
+import MainNav from "@/components/MainNav.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    MainNav,
+  },
 };
 </script>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="w-full text-sm">Main Navigation</header>
+</template>
+
+<script>
+// think of this as a configuration for the component.
+export default {
+  name: "MainNav", // Gives formal name to the component.
+};
+</script>
