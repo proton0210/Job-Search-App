f0549446fff607088dd83845350e9553955a9620
Vidit Shah <vidit0210@protonmail.com>
2022-07-09 15:59:29 +0530

    tailwind config

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <h1 class="mt-8 text-4xl text-center">Hello</h1>
-  <MainNav />
+  <main-nav />
 </template>
 
 <script>
 import MainNav from "@/components/MainNav.vue";
+
 export default {
   name: "App",
   components: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,14 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+
   theme: {
     extend: {
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "brand-gray-1": "#dadce0",
       },
     },
     plugins: [],
