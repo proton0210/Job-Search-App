a0daf746e7c56243d2236e3c6e5c8d33a96bfd4d
Vidit Shah <vidit0210@protonmail.com>
2022-07-10 02:17:46 +0530

    Directives

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,7 +4,22 @@
       <div
         class="flex h-full px-8 mx-auto border-b border-solid flex-nowrap border-brand-gray-1"
       >
-        <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
+        <a :href="url" class="flex items-center h-full text-xl">{{
+          company
+        }}</a>
+        <nav class="h-full ml-12">
+          <ul class="flex h-full p-0 m-0 list-none">
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem"
+              class="h-full ml-9 first:ml-0"
+            >
+              <a href="" class="flex items-center h-full py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -17,6 +32,15 @@ export default {
   data() {
     return {
       company: "Bobo Careers",
+      url: "https://careers.google.com/",
+      menuItems: [
+        "Teams",
+        "Locations",
+        "Life at Bobo Corp",
+        "How we hire?",
+        "Students",
+        "Jobs",
+      ],
     };
   },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,16 @@ module.exports = {
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
       },
+
       colors: {
         "brand-gray-1": "#dadce0",
         "brand-blue-1": "#1967d2",
         "brand-green-1": "#137333",
+      },
+    },
+    variants: {
+      extend: {
+        margin: ["first"],
       },
     },
     plugins: [],
