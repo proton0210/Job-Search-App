327b5ecd6e18cbd068d598e7b891e92dcd967421
Vidit Shah <vidit0210@protonmail.com>
2022-07-10 03:19:36 +0530

    Section 8 complete

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -6,4 +6,18 @@ describe("MainNav.vue", () => {
     const wrapper = mount(MainNav);
     expect(wrapper.text()).toMatch("Bobo Careers");
   });
+
+  it("Displays Menu item for navigation", () => {
+    const wrapper = mount(MainNav);
+    const navigationItems = wrapper.findAll("[data-test='main-nav-list-iem']");
+    const navigationMenuText = navigationItems.map((item) => item.text());
+    expect(navigationMenuText).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Bobo Corp",
+      "How we hire?",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
