556a61adde8b1cfcb4d4b6b4e465886437fbb36e
Vidit Shah <vidit0210@protonmail.com>
2022-07-17 03:47:10 +0530

    LifeCycle

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,14 @@
 <template>
   <main>
     <section class="flex flex-col h-screen pt-10 pb-20 bg-yellow-200">
-      <p>Hero</p>
+      <div class="grid grid-cols-12">
+        <div class="col-span-1 border border-blue-700 cols-start-1">
+          1 column
+        </div>
+        <div class="col-span-5 border-blue-700 cols-start-2">5 cols</div>
+        <div class="col-span-5 border-blue-700 cols-start-7">5 cols</div>
+        <div class="col-span-1 border-blue-700 cols-start-12">1 column</div>
+      </div>
     </section>
   </main>
 </template>
