62a69b515bad53a323bb8b82c3441ee73eb6a1f5
Vidit Shah <vidit0210@protonmail.com>
2022-07-20 03:26:45 +0530

    Job results

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import App from "@/App.vue";
 import "@/assets/tailwind.css";
+import router from "@/router";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { createApp } from "vue";
-import router from "./router";
+// Because the file name is index.js
+
 // Making Font Awesome Global for all components
 library.add(faSearch);
 
